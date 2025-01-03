78eb435f5db5619a5856dbe56c36428f36cbea45
Denis Kolodin <deniskolodin@gmail.com>
2025-01-03 10:53:15 +0100

    Extract a config from the environment

--- i9/i9-core/src/keeper.rs
+++ i9/i9-core/src/keeper.rs
@@ -1,6 +1,12 @@
 use anyhow::Result;
 use async_trait::async_trait;
 use crb::agent::{Agent, AgentSession, OnRequest, Request};
+use serde::de::DeserializeOwned;
+use std::marker::PhantomData;
+
+pub trait Config: DeserializeOwned + Send + 'static {
+    fn namespace() -> &'static str;
+}
 
 pub struct Keeper {}
 
@@ -9,10 +15,20 @@ impl Agent for Keeper {
     type Output = ();
 }
 
-pub struct GetSecret {}
-
-impl Request for GetSecret {
-    type Response = String;
+pub struct GetConfig<C> {
+    _type: PhantomData<C>,
 }
 
-impl OnRequest<GetSecret> for Keeper {}
+impl<C: Config> Request for GetConfig<C> {
+    type Response = C;
+}
+
+#[async_trait]
+impl<C: Config> OnRequest<GetConfig<C>> for Keeper {
+    async fn on_request(&mut self, _: GetConfig<C>, _: &mut Self::Context) -> Result<C> {
+        let mut ns = C::namespace().to_uppercase();
+        ns.push('_');
+        let config: C = envy::prefixed(ns).from_env()?;
+        Ok(config)
+    }
+}
