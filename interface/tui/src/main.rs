3b9607e0cc3981635700361f0fd63cc67fb369fe
Denis Kolodin <deniskolodin@gmail.com>
2025-01-09 07:57:53 +0100

    Init ratatui

--- interface/tui/src/main.rs
+++ interface/tui/src/main.rs
@@ -1,2 +1,63 @@
-fn main() {
+use anyhow::Result;
+use async_trait::async_trait;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
+use crb::agent::{Agent, AgentSession, DoAsync, Next, Runnable};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    TuiApp::new().run().await?;
+
+    /*
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+    */
+
+    Ok(())
 }
+
+struct TuiApp {
+}
+
+impl TuiApp {
+    pub fn new() -> Self {
+        Self {
+        }
+    }
+}
+
+impl Agent for TuiApp {
+    type Context = AgentSession<Self>;
+    type Output = ();
+
+    fn begin(&mut self) -> Next<Self> {
+        Next::do_async(Configure)
+    }
+}
+
+struct Configure;
+
+#[async_trait]
+impl DoAsync<Configure> for TuiApp {
+    async fn once(&mut self, _: &mut Configure) -> Result<Next<Self>> {
+        ratatui::try_init()?;
+        Ok(Next::events())
+    }
+}
+
+/*
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
+}
+*/
