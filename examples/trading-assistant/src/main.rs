405e5fa9fbccb34b55e8b200c1dae8ffd80c781f
Denis Kolodin <deniskolodin@gmail.com>
2025-01-15 14:14:51 +0100

    Implement io reading loop

--- examples/trading-assistant/src/main.rs
+++ examples/trading-assistant/src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use ice_nine_core::Substance;
 use ice_nine_plugin_exchange_dydx::DyDxParticle;
+use ice_nine_plugin_chat_stdio::StdioParticle;
 use ice_nine_plugin_chat_telegram::TelegramParticle;
 use ice_nine_plugin_model_openai::OpenAIParticle;
 
@@ -11,6 +12,7 @@ async fn main() -> Result<()> {
     substance.add_particle::<OpenAIParticle>()?;
     substance.add_particle::<DyDxParticle>()?;
     substance.add_particle::<TelegramParticle>()?;
+    substance.add_particle::<StdioParticle>()?;
     substance.join().await?;
     Ok(())
 }
--- plugins/chat-stdio/src/particle.rs
+++ plugins/chat-stdio/src/particle.rs
@@ -1,12 +1,18 @@
-use crb::agent::{Agent, AgentSession, Next};
+use anyhow::Result;
+use async_trait::async_trait;
+use crb::agent::{Agent, AgentSession, Next, DoAsync, Duty, Context};
 use ice_nine_core::{ParticleSetup, Particle};
+use tokio::io::{stdin, BufReader, Stdin, AsyncBufReadExt, Lines};
 
 pub struct StdioParticle {
+    lines: Lines<BufReader<Stdin>>,
 }
 
 impl Particle for StdioParticle {
     fn construct(setup: ParticleSetup) -> Self {
+        let reader = BufReader::new(stdin());
         Self {
+            lines: reader.lines(),
         }
     }
 }
@@ -16,6 +22,26 @@ impl Agent for StdioParticle {
     type Output = ();
 
     fn begin(&mut self) -> Next<Self> {
-        Next::events()
+        Next::duty(Initialize)
+    }
+}
+
+struct Initialize;
+
+#[async_trait]
+impl Duty<Initialize> for StdioParticle {
+    async fn handle(&mut self, _: Initialize, ctx: &mut Context<Self>) -> Result<Next<Self>> {
+        // TODO: Connect to the substance
+        Ok(Next::do_async(ReadLine))
+    }
+}
+
+struct ReadLine;
+
+#[async_trait]
+impl DoAsync<ReadLine> for StdioParticle {
+    async fn repeat(&mut self, _: &mut ReadLine) -> Result<Option<Next<Self>>> {
+        let line = self.lines.next_line().await?;
+        Ok(None)
     }
 }
