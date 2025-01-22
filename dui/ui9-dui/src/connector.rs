0147bddda5e87939c5d257f294bb1b9c278bc2e6
Denis Kolodin <deniskolodin@gmail.com>
2025-01-22 16:00:54 +0100

    Clean up p2p connector

--- dui/ui9-dui/src/connector.rs
+++ dui/ui9-dui/src/connector.rs
@@ -54,24 +54,18 @@ impl Duty<Initialize> for Connector {
             )?
             .with_quic()
             .with_behaviour(|key| {
-                // To content-address message, we can take the hash of message and use it as an ID.
-                let message_id_fn = |message: &gossipsub::Message| {
+                let unique_message = |message: &gossipsub::Message| {
                     let mut s = DefaultHasher::new();
                     message.data.hash(&mut s);
                     gossipsub::MessageId::from(s.finish().to_string())
                 };
 
-                // Set a custom gossipsub configuration
                 let gossipsub_config = gossipsub::ConfigBuilder::default()
-                    .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-                    .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
-                    // signing)
-                    .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
-                    .build()
-                    .map_err(Error::msg)?; // Temporary hack because `build` does not return a proper `std::error::Error`.
-                                           // .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
+                    .heartbeat_interval(Duration::from_secs(10))
+                    .validation_mode(gossipsub::ValidationMode::Strict)
+                    .message_id_fn(unique_message)
+                    .build()?;
 
-                // build a gossipsub network behaviour
                 let gossipsub = gossipsub::Behaviour::new(
                     gossipsub::MessageAuthenticity::Signed(key.clone()),
                     gossipsub_config,
@@ -84,7 +78,7 @@ impl Duty<Initialize> for Connector {
 
                 let request_response = request_response::cbor::Behaviour::new(
                     [(
-                        StreamProtocol::new("/file-exchange/1"),
+                        StreamProtocol::new("/ui9-trace/0.0.1"),
                         ProtocolSupport::Full,
                     )],
                     request_response::Config::default(),
@@ -98,12 +92,9 @@ impl Duty<Initialize> for Connector {
             })?
             .build();
 
-        // Create a Gossipsub topic
         let topic = gossipsub::IdentTopic::new("ice-nine-ui9");
-        // subscribes to our topic
         swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
 
-        // Listen on all interfaces and whatever port the OS assigns
         swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
         swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
@@ -124,13 +115,13 @@ impl DoAsync<EventLoop> for Connector {
             event = swarm.select_next_some() => match event {
                 SwarmEvent::Behaviour(Ui9BehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
                     for (peer_id, _multiaddr) in list {
-                        println!("mDNS discovered a new peer: {peer_id}");
+                        println!("UI9 node connected: {peer_id}");
                         swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
                     }
                 },
                 SwarmEvent::Behaviour(Ui9BehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
                     for (peer_id, _multiaddr) in list {
-                        println!("mDNS discover peer has expired: {peer_id}");
+                        println!("UI9 node disconnected: {peer_id}");
                         swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
                     }
                 },
