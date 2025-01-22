059b499c7cc243b95b66d0f667fc4d1e8473d8e3
Denis Kolodin <deniskolodin@gmail.com>
2025-01-22 17:25:11 +0100

    Refactor progress tracer

--- dui/ui9-tracers/src/progress.rs
+++ dui/ui9-tracers/src/progress.rs
@@ -3,16 +3,12 @@ use ui9::names::Fqn;
 use ui9_dui::Tracer;
 use ui9_flow::Flow;
 
-#[derive(Serialize, Clone)]
-pub struct ProgressValue {
-    progress: u32,
-}
-
 pub struct Progress {
     tracer: Tracer<ProgressState>,
     current: u64,
     total: u64,
-    value: ProgressValue,
+    /// A calculated value with `current * precision / total`
+    progress: u32,
     /// Adds extra digits after `.`
     precision: u64,
 }
@@ -21,24 +17,23 @@ impl Progress {
     pub fn new(fqn: Fqn, total: u64) -> Self {
         let state = ProgressState { value: 0.0 };
         let tracer = Tracer::new(fqn, state);
-        let value = ProgressValue { progress: 0 };
         Self {
             tracer,
             current: 0,
             total,
-            value,
+            progress: 0,
             precision: 100, // 100.00
         }
     }
 
     pub fn set_value(&mut self, value: u64) {
         self.current = value;
-        let prev_value = self.value.progress;
+        let prev_value = self.progress;
         let new_value = (self.current * 100 * self.precision / self.total) as u32;
         if prev_value != new_value {
-            self.value.progress = new_value;
+            self.progress = new_value;
             let event = ProgressEvent::SetProgress {
-                value: self.value.progress as f32 / self.precision as f32,
+                value: self.progress as f32 / self.precision as f32,
             };
             self.tracer.event(event);
         }
