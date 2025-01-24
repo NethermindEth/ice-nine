661796ca8077575882cb57bc6e2e8f3a42133bf6
Denis Kolodin <deniskolodin@gmail.com>
2025-01-24 13:27:45 +0100

    Track relations in the publisher

--- dui/ui9-dui/src/publisher/server.rs
+++ dui/ui9-dui/src/publisher/server.rs
@@ -6,7 +6,7 @@ use async_trait::async_trait;
 use crb::agent::{Address, Agent, Context, Duty, Next, OnEvent, Standalone};
 use crb::core::Slot;
 use crb::runtime::{InteractiveRuntime, ReachableContext, Runtime};
-use crb::superagent::{OnRequest, Request, Supervisor, SupervisorSession};
+use crb::superagent::{OnRequest, Relation, Request, Supervisor, SupervisorSession};
 use derive_more::{Deref, DerefMut, From};
 use std::collections::HashMap;
 use ui9::names::FlowId;
@@ -36,6 +36,7 @@ pub struct HubServer {
     tree: Slot<Tree>,
     connector: Address<Connector>,
     recorders: HashMap<FlowId, RecorderLink>,
+    relations: HashMap<Relation<Self>, FlowId>,
 }
 
 impl HubServer {
@@ -44,6 +45,7 @@ impl HubServer {
             tree: Slot::empty(),
             connector,
             recorders: HashMap::new(),
+            relations: HashMap::new(),
         }
     }
 }
@@ -58,6 +60,13 @@ pub enum Group {
 
 impl Supervisor for HubServer {
     type GroupBy = Group;
+
+    fn finished(&mut self, rel: &Relation<Self>, ctx: &mut Context<Self>) {
+        if let Some(flow_id) = self.relations.remove(rel) {
+            self.recorders.remove(&flow_id);
+            // TODO: Remove from the tree as well
+        }
+    }
 }
 
 impl Agent for HubServer {
@@ -89,8 +98,9 @@ pub struct Delegate {
 impl OnEvent<Delegate> for HubServer {
     async fn handle(&mut self, delegate: Delegate, ctx: &mut Context<Self>) -> Result<()> {
         let flow_id = FlowId::new(); // TODO: Slab can be used here
-        ctx.spawn_trackable(delegate.runtime, Group::Relay);
+        let rel = ctx.spawn_trackable(delegate.runtime, Group::Relay);
         self.recorders.insert(flow_id, delegate.link);
+        self.relations.insert(rel, flow_id);
         // TODO: Add to the aliases tree
         Ok(())
     }
