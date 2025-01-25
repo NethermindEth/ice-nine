cfa6ee18e80f2fd0117383b3f2b905661e77e305
Denis Kolodin <deniskolodin@gmail.com>
2025-01-25 10:34:36 +0100

    Forward message from the bridge

--- dui/ui9-dui/src/subscriber/bridge.rs
+++ dui/ui9-dui/src/subscriber/bridge.rs
@@ -1,20 +1,39 @@
 use std::sync::LazyLock;
 use crb::core::{mpsc, sync::Mutex};
+use crb::agent::{OnEvent, TheEvent, ToAddress};
 use crate::subscriber::client::Delegate;
 
-static SUB_BRIDGE: LazyLock<SubBridge<Delegate>> = LazyLock::new(|| SubBridge::new());
+static SUB_BRIDGE: LazyLock<EventBridge<Delegate>> = LazyLock::new(|| EventBridge::new());
 
-pub struct SubBridge<T> {
+pub struct EventBridge<T> {
     tx: mpsc::UnboundedSender<T>,
     rx: Mutex<Option<mpsc::UnboundedReceiver<T>>>,
 }
 
-impl<T> SubBridge<T> {
+impl<T> EventBridge<T>
+where
+    T: TheEvent,
+{
     pub fn new() -> Self {
         let (tx, rx) = mpsc::unbounded_channel();
         Self {
             tx,
             rx: Mutex::new(Some(rx)),
         }
+    }
+
+    pub fn subscribe<A>(&'static self, addr: impl ToAddress<A>)
+    where
+        A: OnEvent<T>,
+    {
+        let address = addr.to_address();
+        crb::core::spawn(async move {
+            let rx = self.rx.lock().await.take();
+            if let Some(mut rx) = rx {
+                while let Some(event) = rx.recv().await {
+                    address.event(event).ok();
+                }
+            }
+        });
     }
 }
