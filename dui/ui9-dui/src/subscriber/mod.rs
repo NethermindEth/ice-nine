8ff58b8b19ff0c809735bc09056c35ba2fbf07c0
Denis Kolodin <deniskolodin@gmail.com>
2025-01-28 14:19:16 +0100

    Add the Subscriber trait

--- dui/ui9-dui/src/subscriber/mod.rs
+++ dui/ui9-dui/src/subscriber/mod.rs
@@ -10,7 +10,28 @@ pub use local_player::LocalPlayer;
 
 use crate::flow::Flow;
 use crb::core::{mpsc, watch};
+use derive_more::{Deref, DerefMut};
+use libp2p::PeerId;
 use ui9::names::Fqn;
+
+pub trait Subscriber: Flow + Default {
+    type Driver: From<Listener<Self>>;
+}
+
+#[derive(Deref, DerefMut)]
+pub struct Sub<P: Subscriber> {
+    driver: P::Driver,
+}
+
+impl<P: Subscriber> Sub<P> {
+    pub fn new(peer: Option<PeerId>, fqn: Fqn) -> Self {
+        let state = P::default();
+        let tracer = Listener::<P>::new(peer, fqn);
+        Self {
+            driver: P::Driver::from(tracer),
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum Ported<F> {
