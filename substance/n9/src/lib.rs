60e00832b07a146099fa5d57c332e333181b5771
Denis Kolodin <deniskolodin@gmail.com>
2025-02-03 21:17:05 +0100

    Add a substance to the n9 crate

--- substance/n9/src/lib.rs
+++ substance/n9/src/lib.rs
@@ -1,1 +1,12 @@
+use crb::agent::Agent;
+use crb::superagent::{Supervisor, SupervisorSession};
 
+pub struct Substance {}
+
+impl Supervisor for Substance {
+    type GroupBy = ();
+}
+
+impl Agent for Substance {
+    type Context = SupervisorSession<Self>;
+}
