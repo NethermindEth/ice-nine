77520cc034bf400f95aeea41f940e9892d3132f0
Denis Kolodin <deniskolodin@gmail.com>
2025-02-17 19:28:11 +0100

    Render spinner for active operations

--- particles/app-tui/src/widgets/job_list.rs
+++ particles/app-tui/src/widgets/job_list.rs
@@ -9,17 +9,23 @@ use ratatui::{
 use ui9_app::{Ported, PortedExt};
 use ui9_dui::tracers::job::Job;
 use ui9_dui::{State, Sub};
+use ui9_tui::Spinner;
 
 pub struct JobList {
     job: Sub<Job>,
     state: State<Ported<Job>>,
+    spinner: Spinner,
 }
 
 impl JobList {
     pub fn new() -> Self {
         let mut job = Sub::<Job>::local_unified();
         let state = job.ported_state().unwrap();
-        Self { job, state }
+        Self {
+            job,
+            state,
+            spinner: Spinner::new(),
+        }
     }
 }
 
@@ -36,14 +42,18 @@ impl Component for JobList {
             return Err(r#"No active jobs ʕ•́ᴥ•̀ʔ"#.into());
         }
 
+        let white = Style::default().fg(Color::White);
+        let blue = Style::default().fg(Color::Blue);
         let items: Vec<ListItem> = state
             .operations
             .iter()
             .map(|(_id, record)| {
-                ListItem::new(Line::from(vec![Span::styled(
-                    &record.task,
-                    Style::default().fg(Color::White),
-                )]))
+                let spinner = self.spinner.spinner_char();
+                ListItem::new(Line::from(vec![
+                    Span::styled(spinner.to_string(), blue),
+                    Span::from(" "),
+                    Span::styled(&record.task, white),
+                ]))
             })
             .collect();
 
