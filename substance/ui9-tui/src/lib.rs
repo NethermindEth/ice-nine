9d1586f3b657c1a2de70aaf49893784e22dfd4e1
Denis Kolodin <deniskolodin@gmail.com>
2025-02-17 18:47:22 +0100

    Move the spinner to the separate mod

--- substance/ui9-tui/src/lib.rs
+++ substance/ui9-tui/src/lib.rs
@@ -1,1 +1,27 @@
+use std::time::Instant;
 
+static RATE: u64 = 200;
+static CHARS: &[char] = &['⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽', '⣾'];
+
+pub struct Spinner {
+    started: Instant,
+    rate: u64,
+    chars: Box<[char]>,
+}
+
+impl Spinner {
+    pub fn new() -> Self {
+        Self {
+            started: Instant::now(),
+            rate: RATE,
+            chars: CHARS.into(),
+        }
+    }
+
+    pub fn spinner_char(&self) -> char {
+        let elapsed = self.started.elapsed().as_millis() as u64;
+        let len = self.chars.len() as u64;
+        let idx = elapsed / self.rate % len;
+        self.chars[idx as usize]
+    }
+}
