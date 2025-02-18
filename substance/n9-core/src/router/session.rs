0dd8bc96234174940d307a8504e66dc71f8ab71e
Denis Kolodin <deniskolodin@gmail.com>
2025-02-18 16:31:36 +0100

    Add the chat method to the SessionLink

--- substance/n9-core/src/router/session.rs
+++ substance/n9-core/src/router/session.rs
@@ -1,8 +1,50 @@
-use super::{ChatRequest, ChatResponse, RouterLink};
-use anyhow::{anyhow, Result};
+use super::{ChatRequest, ChatResponse, ReasoningRouter, RouterLink};
+use anyhow::{anyhow, Error, Result};
 use async_trait::async_trait;
-use crb::agent::{Agent, AgentSession, Context, Next};
-use crb::superagent::{Interaction, OnRequest};
+use crb::agent::{Address, Agent, AgentSession, Context, Equip, Next, StopAddress, ToAddress};
+use crb::superagent::{Fetcher, InteractExt, Interaction, OnRequest, Request};
+use derive_more::{Deref, DerefMut, From};
+
+impl RouterLink {
+    pub async fn new_session(&self) -> Result<SessionLink> {
+        self.interact(NewSession).await.map_err(Error::from)
+    }
+}
+
+struct NewSession;
+
+impl Request for NewSession {
+    type Response = SessionLink;
+}
+
+#[async_trait]
+impl OnRequest<NewSession> for ReasoningRouter {
+    async fn on_request(&mut self, _: NewSession, ctx: &mut Context<Self>) -> Result<SessionLink> {
+        let link = ctx.equip();
+        let session = ReasoningSession::new(link);
+        let addr = ctx.spawn_agent(session, ());
+        Ok(addr.equip())
+    }
+}
+
+#[derive(Deref, DerefMut)]
+pub struct SessionLink {
+    address: StopAddress<ReasoningSession>,
+}
+
+impl From<Address<ReasoningSession>> for SessionLink {
+    fn from(address: Address<ReasoningSession>) -> Self {
+        Self {
+            address: address.to_stop_address(),
+        }
+    }
+}
+
+impl SessionLink {
+    pub fn chat(&self, request: ChatRequest) -> Fetcher<ChatResponse> {
+        self.interact(request)
+    }
+}
 
 pub struct ReasoningSession {
     router: RouterLink,
