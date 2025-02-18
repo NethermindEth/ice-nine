06ab90aae4c2d45b976726b72fc69211df881eec
Denis Kolodin <deniskolodin@gmail.com>
2025-02-18 10:28:23 +0100

    Add a chat request handle to the session router

--- substance/n9-core/src/router/session.rs
+++ substance/n9-core/src/router/session.rs
@@ -1,5 +1,8 @@
-use super::RouterLink;
-use crb::agent::{Agent, AgentSession, Next};
+use super::{ChatRequest, RouterLink};
+use anyhow::Result;
+use async_trait::async_trait;
+use crb::agent::{Agent, AgentSession, Context, Next};
+use crb::superagent::{Interaction, OnRequest};
 
 pub struct ReasoningSession {
     router: RouterLink,
@@ -10,5 +13,17 @@ impl Agent for ReasoningSession {
 
     fn begin(&mut self) -> Next<Self> {
         Next::events()
+    }
+}
+
+#[async_trait]
+impl OnRequest<ChatRequest> for ReasoningSession {
+    async fn handle(
+        &mut self,
+        lookup: Interaction<ChatRequest>,
+        ctx: &mut Context<Self>,
+    ) -> Result<()> {
+        // ctx.do_next(Next::do_async(SendingRequest));
+        Ok(())
     }
 }
